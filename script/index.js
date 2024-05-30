a677c2728a783bb6cbb7de4297ef1ac0f885aa27
SERHII <4sergeyvoropaev@gmail.com>
2024-05-30 18:15:42 +0200

    added all files

---
+++ script/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("partialsLoaded", () => {
+    import("./accelerate-ai.js");
+    import("./header.js");
+    import("./review.js");
+    import("./header-intro.js");
+    import("./form.js");
+});
